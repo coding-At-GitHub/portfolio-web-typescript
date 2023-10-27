b30267a32b61d9b2b10e9dcd1195b7939a8657bb
Rajesh Choudhary <101490518+coding-At-GitHub@users.noreply.github.com>
2023-10-27 18:58:17 +0530

    Update home.jsx

--- src/Components/home.jsx
+++ src/Components/home.jsx
@@ -206,7 +206,10 @@ function MainPage() {
                 <section >
                     <h2>About Me</h2>
                     <p>
-                      1+ Year Exprience in frontend Reactjs 
+                      Passionate Software Engineer with 1+ year of experience in web, frontend and backend development in both Angular And ReactJs in
+frontend. Skilled at writing clear, concise code that is easy to maintain and troubleshoot. Experienced in working with both small and
+large teams across multiple projects and companies. Able to work independently of remote locations or in office environments as
+needed by the company. 
                     </p>
                    
                 </section>
