444e6cf0bf2651a7537aab992d50b1168353851e
Rajesh Choudhary <rjsh1503@gmail.com>
2023-11-03 10:03:16 +0530

    Update EducationSect.jsx

--- src/Components/sections/EducationSect.jsx
+++ src/Components/sections/EducationSect.jsx
@@ -1,31 +1,26 @@
 import React from 'react';
 import './../CombinedCss.css';
 import { Grid, Typography, Card, CardContent, CardActions, Button } from '@material-ui/core';
+import Stack from '@mui/material/Stack';
+
 function EducationSect() {
   return (
     <div>
-      <Grid  xs={12} sm={6} md={4}>
-            <Card>
-         
-              <CardContent>
-                <Typography variant="h6">BTech</Typography>
-                <Typography variant="body2">Govt Engineering college, Ajmer, Rajasthan</Typography>
-              </CardContent>
-             
-            </Card>
-            <Card>
-         
-              <CardContent>
-                <Typography variant="h6">Scholing</Typography>
-                <Typography variant="body2">Kendriya Vidyalaya NTPC Anta, Rajasthan</Typography>
-              </CardContent>
-             
-            </Card>
-          
-          </Grid>
-          <Grid   xs={12} sm={6} md={4}>
-           
-          </Grid>
+      <Stack direction="row" spacing={2}>
+        <Card>
+          <CardContent>
+            <Typography variant="h6">BTech</Typography>
+            <Typography variant="body2">Govt Engineering college, Ajmer, Rajasthan</Typography>
+          </CardContent>
+        </Card>
+        <Card>
+          <CardContent>
+            <Typography variant="h6">Scholing</Typography>
+            <Typography variant="body2">Kendriya Vidyalaya NTPC Anta, Rajasthan</Typography>
+          </CardContent>
+        </Card>
+      </Stack>
+
     </div>
   )
 }
