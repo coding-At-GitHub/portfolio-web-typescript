85ee458d62d267052e9b1c7ceefe9fc9368a5859
Rajesh Choudhary <rjsh1503@gmail.com>
2023-11-08 16:36:23 +0530

    updated

--- src/Components/sections/ProjectCard.jsx
+++ src/Components/sections/ProjectCard.jsx
@@ -1,37 +1,38 @@
 import * as React from 'react';
 import Card from '@mui/material/Card';
+import Stack from '@mui/material/Stack';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
+import Chip from '@mui/material/Chip';
 import './../combined.css';
+
 
 export default function ProjectCard({ title, description, imageUrl, projectUrl, sourceCodeUrl }) {
     return (
         <>
-
-            <Card sx={{ maxWidth: 215, ml: 2, backgroundColor:'#badee8' }} className='projectCard'>
-                <CardMedia
-                    component="img"
-                    alt={title}
-                    height="100"
-                    image={imageUrl}
-                />
+            <Card sx={{ maxWidth: 215, ml: 2, backgroundColor: '#badee8' }} className='projectCard'>
+                <CardMedia component="img" alt={title} image={imageUrl} height="100" />
                 <CardContent>
-                    <Typography gutterBottom variant="h5" component="div">
-                        {title}
-                    </Typography>
-                    <Typography variant="body2" color="text.secondary">
-                        {description}
-                    </Typography>
+                    <Typography gutterBottom variant="h5" component="div"> {title} </Typography>
+                    <Typography variant="body2" color="text.secondary"> {description} </Typography>
                 </CardContent>
-                <CardActions>
-                    <Button size="small">View Project</Button>
-                    <Button size="small">Source Code</Button>
+                <CardActions >
+                    <Stack
+                        className="project-links"
+                        direction="row"
+                        justifyContent="flex-start"
+                        alignItems="center"
+                        spacing={1}
+                    >
+                        <Chip label="View Project" component="a" href={projectUrl} clickable size="small" />
+                        <Chip label="Source Code" component="a" href={sourceCodeUrl} clickable size="small" />
+                    </Stack>
+
                 </CardActions>
             </Card>
-
 
         </>
     );
--- src/Components/sections/ProjectTwoSect.jsx
+++ src/Components/sections/ProjectTwoSect.jsx
@@ -10,12 +10,13 @@ import SSAlgoSort from './../../assets/SortAlgo.png';
 
 const projects = [
   {
-    title: 'Portfolio',
-    description: 'Description of Skills and Project.Easy to share Link instead of ',
-    imageUrl: SSPortfolio,
-    projectUrl: 'https://cggb6112.vercel.app/',
+    title: 'Resume Creator',
+    description: 'This is a website that users can use to build their resumes. Description: Users have to fill in their details, choose a template   and download the final resume',
+    imageUrl: SSResumeCreator,
+    projectUrl: 'https://isrodesigns.netlify.app/',
     sourceCodeUrl: 'https://github.com/coding-At-GitHub',
   },
+ 
   {
     title: 'Memory Game',
     description: 'Each card has a picture on one side and is duplicated. The game begins with all of the cards face down, and the player turns over two cards in turn. They keep the cards if they have the same image otherwise, they turn them face down again.',
@@ -23,18 +24,19 @@ const projects = [
     projectUrl: 'https://flip-the-tile-game.netlify.app/',
     sourceCodeUrl: 'https://github.com/coding-At-GitHub',
   },
-  {
-    title: 'Resume Creator',
-    description: 'This is a website that users can use to build their resumes. Description: Users have to fill in their details, choose a template   and download the final resume',
-    imageUrl: SSResumeCreator,
-    projectUrl: 'https://isrodesigns.netlify.app/',
-    sourceCodeUrl: 'https://github.com/coding-At-GitHub',
-  },
+ 
   {
     title: 'Visual Algo Sorting',
     description: 'This webpage helps a learner to under different sorting algorithms in visual manner and easy to understand. As sorting is basic and important feature used in almost all product based companies',
     imageUrl: SSAlgoSort,
     projectUrl: 'https://visual-algorithms-react.vercel.app/',
+    sourceCodeUrl: 'https://github.com/coding-At-GitHub',
+  },
+  {
+    title: 'Portfolio',
+    description: 'Description of Skills and Project.Easy to share Link instead of ',
+    imageUrl: SSPortfolio,
+    projectUrl: 'https://cggb6112.vercel.app/',
     sourceCodeUrl: 'https://github.com/coding-At-GitHub',
   },
 
